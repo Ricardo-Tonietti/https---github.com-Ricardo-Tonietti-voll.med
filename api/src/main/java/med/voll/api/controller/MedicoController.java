e510fa50bfeb125df2e3fadcd3fef24f927a463e
Usuario <Usuario@RICARDO-NITRO>
2023-06-21 08:51:19 -0300

    create method Get, page Medicos e Paciente

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,8 +3,12 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,10 +24,15 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados ){
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 
         repository.save(new Medico(dados));
         System.out.println(dados);
+    }
 
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
+
+        return repository.findAll(pageable).map(DadosListagemMedico::new);
     }
 }
